c3f3053e57ce289e2bcc8760af239b7b8a7fbad8
Ola Assem <o.assem@gmail.com>
2018-03-14 21:49:24 +0200

    added models folder

--- routes/routine.js
+++ routes/routine.js
@@ -15,14 +15,9 @@ router.get('/week', (req, res) => {
 
 
 
-//start exporting the router
-//has to be the last code (use this order)
+//EXPORT THE ROUTER
+//Must be the last code (use this order)
 module.exports = router;
 
 
 
-//create model and modelSchema for the routine
-//think of new models (user, gym, profile, etc)
-//make basic endpoints (Create, getById, Delete, etc)
-//connect with database / connect mongolab with app
-//serve static files (Jon)
